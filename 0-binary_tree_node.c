9a8f9c026654fbc03894c724d88ff0f6bf66251e
ZorEl212 <yabsirad212@gmail.com>
2023-11-08 06:27:26 -0500

    Betty: Fix Betty errors for some files/functions

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,17 +1,24 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - add a node in binary tree
+ * @parent: Partnt node of the newly created node
+ * @value: value of the new node
+ *
+ * Return: The new node
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *child = malloc(sizeof(binary_tree_t));
+binary_tree_t *child = malloc(sizeof(binary_tree_t));
 
-    if (!child)
-        return NULL;
+	if (!child)
+		return (NULL);
 
-    child->left = NULL;
-    child->left = NULL;
-    child->parent = NULL;
-    if (parent)
-        child->parent = parent;
-    child->n = value;
-    return child;
+	child->left = NULL;
+	child->left = NULL;
+	child->parent = NULL;
+	if (parent)
+		child->parent = parent;
+	child->n = value;
+	return (child);
 }

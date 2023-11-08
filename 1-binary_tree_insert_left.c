2eb0bea8d18eb1a43a6ecad73c3d5bbb5dcda06c
ZorEl212 <yabsirad212@gmail.com>
2023-11-08 07:33:01 -0500

    Insert Left: Fix unnecessary malloc to handle leak

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,20 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inset a left node in a tree
- * @parent: parent node
- * @value: value that the new node holds
- *
- * Return: The new node
+* binary_tree_insert_left - inset a left node in a tree
+* @parent: parent node
+* @value: value that the new node holds
+*
+* Return: The new node
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *child = malloc(sizeof(binary_tree_t));
+	binary_tree_t *child = NULL;
 
-	if (!child || !parent)
+	if (!parent)
 		return (NULL);
-
 	child = binary_tree_node(parent, value);
+	if (!child)
+		return (NULL);
 	if (parent->left)
 	{
 		child->left = parent->left;

581a988285fd17013216a828108ae88f254c3ded
ZorEl212 <yabsirad212@gmail.com>
2023-11-08 06:19:43 -0500

    Binary Tree: Header file for the whole project 	Add a function to create a node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *child = malloc(sizeof(binary_tree_t));
+
+    if (!child)
+        return NULL;
+
+    child->left = NULL;
+    child->left = NULL;
+    child->parent = NULL;
+    if (parent)
+        child->parent = parent;
+    child->n = value;
+    return child;
+}

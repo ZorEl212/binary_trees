71a763cba3f3458f686b428463e5dc59022e8144
ZorEl212 <yabsirad212@gmail.com>
2023-11-10 08:57:32 -0500

    Binary Tree: Calculate binary tree size
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_size - calculate depth of a binary tree
+* @tree: binary tree
+*
+* Return: Height of the tree
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size_left, size_right;
+
+	if (!tree)
+		return (0);
+
+	size_left = binary_tree_size(tree->left);
+	size_right = binary_tree_size(tree->right);
+
+	return (1 + size_left + size_right);
+}
